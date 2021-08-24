1cb87ac25e0ac6729f5ef28c7b298667e84c1457
buingocchinh321 <63283198+buingocchinh321@users.noreply.github.com>
2021-08-25 01:38:13 +0700

    add 2 func in DBHandler
    changePass and changeInfor

--- Server/src/com/p2p/p4f/server/DBCPConnectionPoolingExample.java
+++ Server/src/com/p2p/p4f/server/DBCPConnectionPoolingExample.java
@@ -4,36 +4,26 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 
-
 public class DBCPConnectionPoolingExample {
 
     private static final int NUMBER_OF_USERS = 5;
 
     public static void main(String[] args) throws SQLException, InterruptedException {
         ConnectionPool.setFirstUrl("jdbc:sqlserver://NChinh-Laptop\\SQLEXPRESS;databaseName=PrayForFood;integratedSecurity=true;");
-        Thread T1 = new Thread(()->{
             try(Connection conn = ConnectionPool.getConnection();
                 ){
                 DBHandler dbH = new DBHandler(conn);
-            System.out.println("Thread 1 " + dbH.Login("khachhang2","pss"));
-            conn.close();}
+                Int_User rs =  dbH.Login("khachhang2","pss");
+            System.out.println("Thread 1 " + rs.getInt());
+                System.out.println("Thread 1 " + rs.u.username);
+                dbH.getNewConn();
+                User newU = new User (rs.u);
+                newU.email = "kh2@mail.vn";
+            System.out.println("Thread 1 " + dbH.ChangeInformation(rs.u,rs.u.pass,newU));
+                }
             catch(SQLException e){
                 e.printStackTrace();
             }
-        });
-        Thread T2 = new Thread(()->{
-            try(Connection conn = ConnectionPool.getConnection();
-            ){
-                DBHandler dbH = new DBHandler(conn);
-                System.out.println("Thread 2 " + dbH.Login("khachhang3","pss"));
-                conn.close();}
-            catch(SQLException e) {
-                e.printStackTrace();
-            }
-        });
-        T1.start();
-        T2.start();
-        T1.join();
-        T2.join();
+
     }
 }
--- Server/src/com/p2p/p4f/server/DBHandler.java
+++ Server/src/com/p2p/p4f/server/DBHandler.java
@@ -27,6 +27,14 @@ class User{
         this.addr = addr;
         this.image = image;
     }
+    public User(Object o){
+        this.username = ((User) o).username;
+        this.pass = ((User) o).pass;
+        this.email = ((User) o).email;
+        this.phone = ((User) o).phone;
+        this.addr = ((User) o).addr;
+        this.image = ((User) o).image;
+    }
 }
 
 class Restaurant{
@@ -48,32 +56,56 @@ class Restaurant{
     }
 }
 
+class Int_User{
+    public int i;
+    public User u;
+    public Int_User(){}
+    public Int_User(int i, User u){
+        this.i = i;
+        this.u = u;
+    }
+    public int getInt() {return this.i;}
+    public User getUser(){return this.u;}
+    public void setInt(int i){this.i = i;}
+    public void setUser(User u){this.u = u;}
+}
 
+//Each time using the new query, you have to create new dbhandler
 public class DBHandler {
     private Connection conn;
     public DBHandler(Connection conn){
         this.conn = conn;
     }
 
+
     // list[0]:  0 = user login, 1 = username fault, 2 = pw fault, 3 = staff login, 4 = other fault
     // list[1]:  User information
-    public List<Object> Login(String user, String pass) {
+    public Int_User Login(String user, String pass) {
         String sqlState = "select * from tblUser u where u.Username = \'" + user+"\'";
         User u = new User();
+        Int_User Result = new Int_User();
         try (   Connection conn = this.conn;
                 PreparedStatement st = conn.prepareStatement(sqlState);
                 ResultSet rs = st.executeQuery();
                 ) {
-            if (!rs.next()) return Arrays.asList(1, u);
-            if (!pass.equals(rs.getString("U_pass"))) {
-                return Arrays.asList(2, u);
+            if (!rs.next()) Result.setInt(1);
+            if (!pass.equals(rs.getString("U_pass"))) Result.setInt(2);
+            if (rs.getString("Usertype").equals("1")) {
+                Result.setInt(0);
+                u = new User(rs.getString(1), rs.getString(2),
+                        rs.getString(4),rs.getString(5),
+                        rs.getString(6),rs.getString(7));
             }
-            if (rs.getString("Usertype").equals("1")) return Arrays.asList(0, u);
-            else return Arrays.asList(3, u);
+            else Result.setInt(3);
+            st.close();
+            rs.close();
+            //conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return Arrays.asList(4, u);
+        Result.setUser(u);
+
+        return Result;
     }
 
     // 0 = no fault, 1 = user fault, 2 = email fault, 3 = phone fault
@@ -148,6 +180,64 @@ public class DBHandler {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public boolean ChangePassword (User user,String oldPass,String newPass){
+        // if the inputted oldPass is incorrect or the new pass and old pass is the same
+        System.out.println(user.pass + "  " + oldPass + "  " + newPass);
+        if ((!user.pass.equals(oldPass))|| user.pass.equals(newPass)) return false;
+
+        String sqlUpdate = "Update tblUser\n" +
+                "set U_pass = " + "\'" + newPass + "\'" +
+                "\nwhere Username = " + "\'" + user.username + "\'";
+        try(
+                Connection conn = this.conn;
+                PreparedStatement sql = conn.prepareStatement(sqlUpdate);
+                ){
+            System.out.println(sqlUpdate);
+            System.out.println(user.username);
+            sql.executeUpdate();
+            conn.commit();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean ChangeInformation (User user, String oldPass, User newInfo){
+        // if the inputted oldPass is incorrect
+
+        if (!user.pass.equals(oldPass) ) return false;
+
+        String sqlUpdate = "Update tblUser\n" +
+                "set Email = ?,\n" +
+                "Phone = ?,\n" +
+                "U_address = ?,\n" +
+                "U_image = ?\n" +
+                "where Username = ?";
+        Connection conn = this.conn;
+        try(
+                PreparedStatement sql = conn.prepareStatement(sqlUpdate);
+        ){
+
+            sql.setString(1, newInfo.email );
+            sql.setString(2, newInfo.phone );
+            sql.setString(3,newInfo.addr );
+            sql.setString(4, newInfo.image );
+            sql.setString(5, user.username );
+            System.out.println(sqlUpdate);
+            sql.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void getNewConn() throws SQLException {
+        this.conn = ConnectionPool.getConnection();
     }
 }
 
