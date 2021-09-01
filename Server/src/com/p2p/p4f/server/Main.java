b29292a09dfc4bf5b6b0494ebd72a87736f375ed
nvakiet <63222542+nvakiet@users.noreply.github.com>
2021-09-01 21:40:34 +0700

    Change to input connection string and bind IP when starting Server

--- Server/src/com/p2p/p4f/server/Main.java
+++ Server/src/com/p2p/p4f/server/Main.java
@@ -5,10 +5,15 @@ public class Main {
     
     public static void main(String[] args) {
         try {
-            String dbConnString = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=PrayForFood;integratedSecurity=true;";
-            ServerP4F server = new ServerP4F(dbConnString, "192.168.1.2", 9999);
-            server.start();
             Scanner sc = new Scanner(System.in);
+            System.out.print("Input database connection: ");
+            String dbConnString = sc.nextLine();
+            System.out.print("Input bind IP: ");
+            String ip = sc.nextLine();
+            ServerP4F server = new ServerP4F(dbConnString, ip, 9999);
+            //String dbConnString = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=PrayForFood;integratedSecurity=true;";
+            //ServerP4F server = new ServerP4F(dbConnString, "192.168.1.2", 9999);
+            server.start();
             int code = sc.nextInt();
             if (code == 0)
                 server.closeServer();
